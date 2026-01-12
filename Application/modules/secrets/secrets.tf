685d1cdcc7beb58638b658b306942a267e909cb8
Fayaz01 <32244073+Fayaz01@users.noreply.github.com>
2026-01-12 20:16:18 +0530

    Delete Application directory
    delete application

--- Application/modules/secrets/secrets.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_secretsmanager_secret" "name" {
-    name = var.secret_name
-  
-}
--- Application/modules/secrets/variable.tf
+++
@@ -1,6 +0,0 @@
-variable "secret_name" {
-    type = string
-    description = "Name of the secret manager name"
-    sensitive = true
-  
-}
